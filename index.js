27507636fe92e98928c479b8a74d0d7269aef8ee
Erica Luong <huongie2lu@gmail.com>
2018-11-18 02:28:14 -0500

    Edit:Minor Updates to Syntax

--- index.js
+++ index.js
@@ -12,33 +12,32 @@ const app = new Vue({
   methods: {
     //start count
     handleStart: function() {
-      this.timer = setInterval(() => this.decreaseTimer(), 1000)
+      this.timer = setInterval(() => this.decreaseTimer(), 1000);
     },
 
-    //countdown
+    //timer countdown
     decreaseTimer: function() {
       if (this.remainingTime >= 1) {
         this.remainingTime--;
-        console.log(this.remainingTime)
       }
       else {
         this.remainingTime = 0;
-        this.handlePause()
+        this.handlePause();
         alert("Time's Up!");
       }
     },
 
     //pause count
     handlePause: function() {
-      clearInterval(this.timer)
-      this.timer = null
+      clearInterval(this.timer);
+      this.timer = null;
     },
 
     //reset count
     handleReset: function() {
-      this.remainingTime = 60 * 25
-      clearInterval(this.timer)
-      this.timer = null
+      this.remainingTime = 60 * 25;
+      clearInterval(this.timer);
+      this.timer = null;
     }
   },
 
@@ -47,13 +46,13 @@ const app = new Vue({
     minutes: function() {
       const minutes = Math.floor(this.remainingTime / 60);
       if (minutes > 10) return minutes
-      else return '0' + minutes
+      else return '0' + minutes;
     },
 
     seconds: function() {
       const seconds = this.remainingTime - (this.minutes * 60);
       if (seconds > 10) return seconds
-      else return '0' + seconds
+      else return '0' + seconds;
     }
   }
 })
